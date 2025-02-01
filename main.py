86226031b13a9a3e7326fe1341087ed306389581
thaimtl <2214926@EDU.VANIERCOLLEGE.QC.CA>
2025-02-01 16:04:43 -0500

    UPDATE1

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn #
+app = FastAPI()
+print("Hello world")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
